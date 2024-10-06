a7445898eb262de870e9251f8e382e9cbc926f02
abhijeet rai <abhijeetrai7985@gmail.com>
2024-10-06 14:39:22 +0530

    cors

--- server/index.js
+++ server/index.js
@@ -1,21 +1,37 @@
 const express = require("express");
 const app = express();
-require("./dbconnection/dbconfig");
-const port =9000;
-
-
-const userRouter = require("./routing/userRouter");
-
+const cors= require('cors');
 const io = require('socket.io')(8585,{
     cors:{
         origin:'http://localhost:3002'
     }
 })
+
+const port =9000;
+
+const userRouter = require("./routing/userRouter");
+require("./dbconnection/dbconfig");
+
+
  
 
 app.use('/user', userRouter);
+app.use(express.json())
+app.use(cors())
 
+io.on('connection', socket => {
 
+    console.log('User connected', socket.id);
+    
+    // socket.on('addUser', userId => {
+    
+    // socket.userId = userId;
+    
+    // });
+    
+    // io.emit('getUsers', socket.userId);
+    
+    });
 
 
 
